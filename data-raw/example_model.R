3e4ad9c0c04142e71ba73cb62759b6b01841be26
Philipp Münch <philipp.muench@helmholtz-hzi.de>
2019-02-13 16:53:00 +0100

    update man

--- data-raw/example_model.R
+++ data-raw/example_model.R
@@ -1,5 +1,5 @@
 library(readr)
-library(h5)
+library(keras)
 
-example_model <- load_model_hdf5("example_model.hdf5")
+example_model <- keras::load_model_hdf5("data-raw/example_model.hdf5")
 use_data(example_model, overwrite = TRUE)
