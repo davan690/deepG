afd90b7896391ca11809dde8d5360a0464b3a7b3
Philipp Münch <philipp.muench@helmholtz-hzi.de>
2019-02-13 19:30:35 +0100

    minor modification to unit test

--- tests/testthat/test-predict.R
+++ tests/testthat/test-predict.R
@@ -4,5 +4,6 @@ test_that("prediction of next character", {
   example_model <- keras::load_model_hdf5("../../data-raw/example_model.hdf5") # model requires length of 30 char
   expect_error(predict_next_nucleotide())
   expect_error(predict_next_nucleotide(sequence = "", model = NULL))
-  expect_s4_class(predict_next_nucleotide("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", example_model), "prediction")
+  expect_s4_class(predict_next_nucleotide(strrep("A",30),
+                                          example_model), "prediction")
 })
