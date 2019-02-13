f502ab7e91e049e80ba91d8bdc909855752ad42f
Philipp Münch <philipp.muench@helmholtz-hzi.de>
2019-02-13 15:01:50 +0100

    add data

---
+++ R/data.R
@@ -0,0 +1,29 @@
+#' Example training dataset consiting of a sequence of nucleotides of CRISPR loci
+#' Filtered for unambigous characters and contains only characters in the vocabulary {A,G,G,T
+#' }
+#' Can be loaded to workspace via data(crispr_sample)
+#' @format Large character of 4.9 Mb
+#' @references \url{http://github.com/philippmuench}
+#' @examples
+#' head(crispr_sample)
+"crispr_sample"
+
+#' Example training dataset consiting of a sequence of nucleotides of CRISPR loci
+#' Filtered for unambigous characters and contains only characters in the vocabulary {A,G,G,T
+#' }
+#' contain all CRISPR loci found in NCBI representative genomes using CRT
+#' Can be loaded to workspace via data(crispr_full)
+#' @format Large character of 4.9 Mb
+#' @references \url{http://github.com/philippmuench}
+#' @examples
+#' head(crispr_full)
+"crispr_full"
+
+
+#' Training dataset of synthetic parenthesis language
+#' Can be loaded to workspace via data(parenthesis)
+#' @format Large character of 4.9 Mb
+#' @references \url{http://github.com/philippmuench}
+#' @examples
+#' head(parenthesis)
+"parenthesis"
