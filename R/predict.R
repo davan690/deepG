a6fcaf65ce442549e4d58d86b49b2e8930a9cb3d
Philipp Münch <philipp.muench@helmholtz-hzi.de>
2019-02-15 18:06:01 +0100

    add one-by-one prediction

--- R/predict.R
+++ R/predict.R
@@ -1,21 +1,24 @@
 #' Predict next nucleotides in sequence output as S4 class
 #'
-#' @param sequence input sequence
+#' @param sequence input sequence, length should be in sync with the model
+#' if length exceeds input_shape of model then only the right side of the
+#' sequence will be used
 #' @param model trained lstm
 #' @param vocabulary ordered vocabulary of input sequence
 #' @export
 predict_next_nucleotide <- function(sequence,
                                     model,
                                     vocabulary = c("\n", "a", "c", "g", "t")){
-  if (missing(sequence))
+  Check <- ArgumentCheck::newArgCheck()
+    if (missing(sequence))
     stop("Need to specify sequence")
   if (missing(model))
     stop("Need to specify model")
   Check <- ArgumentCheck::newArgCheck()
   #* Add an error if sequence length is too small
-  if (nchar(sequence) < 1)
+  if (nchar(sequence) < model$input_shape[2] )
     ArgumentCheck::addError(
-      msg = "'sequence' must be of length > 0",
+      msg = paste0("'sequence' must be of length >", model$input_shape[2]),
       argcheck = Check
     )
   #* Return errors and warnings (if any)
@@ -28,10 +31,17 @@ predict_next_nucleotide <- function(sequence,
              next_char = "character",
              probability = "numeric",
              index = "numeric",
-             alternative_probabilty = "matrix"
+             alternative_probabilty = "matrix",
+             solution = "character"
            )
   )
-  sentence <- sequence %>% stringr::str_to_lower()  %>%
+  substringright <- function(x, n){
+    substr(x, nchar(x)- n + 1, nchar(x))
+  }
+  # sequence can be longer then model input shape
+  # if so just use the last input_shape chars
+  sentence <- substringright(sequence, as.numeric(model$input_shape[2])) %>%
+    stringr::str_to_lower() %>%
     tokenizers::tokenize_characters(strip_non_alphanum = FALSE, simplify = TRUE)
   x <- sapply(vocabulary, function(x){
     as.integer(x == sentence)
@@ -46,5 +56,66 @@ predict_next_nucleotide <- function(sequence,
              next_char = next_char,
              probability = preds[next_index],
              index = next_index,
-             alternative_probabilty = preds))
+             alternative_probabilty = preds,
+             solution = paste0(sequence, next_char)))
+}
+
+#' One beam
+#' @param sequence input sequence
+#' @param model trained lstm
+#' @param vocabulary ordered vocabulary of input sequence
+#' @param num_alternatives number of best solutions
+#' @export
+one_beam <- function(sequence, model, vocabulary, num_alternatives = 2){
+  # getting num_alternative best indexes in vocabulary
+  solutions <- list()
+  confidences <- list()
+  prediction <- predict_next_nucleotide(sequence, model, vocabulary)
+  for (nth_best in 1:num_alternatives){
+    solutions[[nth_best]] <- paste0(sequence, vocabulary[Rfast::nth(prediction@alternative_probabilty, nth_best, descending = T, index.return=T)])
+    confidences[[nth_best]] <- Rfast::nth(prediction@alternative_probabilty, nth_best, descending = T)
+  }
+  return(list(solutions, confidences))
+}
+
+
+#' Replaces specific nucleotides in a sequence one by one
+#' @param sequence input sequence
+#' @param model trained lstm
+#' @param char character that will be replaced
+#' @param vocabulary ordered vocabulary of input sequence
+#' @export
+replace_char <- function(sequence = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAXAAAXAAA",
+                         model,
+                         char = "X",
+                         num_alternative = 2,
+                         vocabulary = c("\n", "a", "c", "g", "t")){
+  require(stringr)
+  Check <- ArgumentCheck::newArgCheck()
+  if (missing(sequence))
+    stop("Need to specify sequence")
+  if (missing(model))
+    stop("Need to specify model")
+  Check <- ArgumentCheck::newArgCheck()
+  #* Add an error if sequence length is too small
+  if (nchar(sequence) < model$input_shape[2] )
+    ArgumentCheck::addError(
+      msg = paste0("'sequence' must be of length >", model$input_shape[2]),
+      argcheck = Check
+    )
+  #* Return errors and warnings (if any)
+  ArgumentCheck::finishArgCheck(Check)
+
+  while (str_detect(sequence, char)) {
+    # get the position
+    next_position <- stringr::str_locate_all(pattern = 'X', sequence)[[1]][[1]]
+    seed <- substr(sequence,
+                       next_position - model$input_shape[[2]] - 1,
+                       next_position - 1)
+    prediction <- predict_next_nucleotide(seed, model, vocabulary)
+    sequence <- paste0(prediction@solution,
+                       substr(sequence, next_position + 1,
+                              nchar(sequence)))
+  }
+  return(sequence)
 }
