3b265c9b3ebb62c0b0f869a72b91ab73eaaa2b2d
Philipp Münch <philipp.muench@helmholtz-hzi.de>
2019-10-28 17:15:54 +0100

    load libraries on startup and check TF

---
+++ R/package.R
@@ -0,0 +1,26 @@
+#' deepG for GenomeNet
+#'
+#' deepG is a is an open source software librar for building deep neuronal 
+#' networks for genomic modeling
+#' 
+#' This package generates \href{http://www.genomenet.de}{GenomeNet}
+#'
+#' For additional documentation on the deepG package see
+#' \href{https://genomenet.de}{https://genomenet.de}
+#'
+#' @import reticulate
+#' @import keras
+#' @import tensorflow
+#'
+#' @docType package
+#' @name deepG
+NULL
+
+# globals
+.globals <- new.env(parent = emptyenv())
+.globals$tensorboard <- NULL
+
+.onLoad <- function(libname, pkgname) {
+  ensure_loaded()
+  message("The deepG package has been successfully. Please see ?deepG for informations.")
+}
