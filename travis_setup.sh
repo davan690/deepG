6c62ee2a3d2fd669cbf109827bbdceab653e0c3a
Philipp Münch <philipp.muench@helmholtz-hzi.de>
2019-10-29 16:39:11 +0100

    add virtualenv

--- travis_setup.sh
+++ travis_setup.sh
@@ -4,6 +4,10 @@
 sudo apt-get install python3-setuptools
 sudo easy_install3 pip
 
+# venv
+sudo apt-get install python3-venv
+pip install --user virtualenv
+
 # hdf5
 cd ..
 wget "$HDF5_RELEASE_URL/hdf5-${HDF5_VERSION%.*}/hdf5-$HDF5_VERSION/src/hdf5-$HDF5_VERSION.tar.gz"
