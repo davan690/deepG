98c7a87755f65e3c93901e9b55f5b07276e8bc38
Philipp Münch <philipp.muench@helmholtz-hzi.de>
2019-11-17 17:13:35 +0100

    update function to TF 2.0

--- R/gpu-management.R
+++ R/gpu-management.R
@@ -1,31 +1,27 @@
 #' Limit Tensorflow proccess to one or multiple GPUs.
 #'
-#' @param gpus GPU ids
+#' @param gpus number of GPU to use (as integer), where first GPU on machine is 1
 #' @param growth memory consumption of GPU grows and will not be blocked
+#' @param env set environmental variable CUDA_VISIBLE_DEVICES
 #' @export
-startGPUSession <- function(gpus = "0", growth = T){
-  require(tensorflow)
-  tf$reset_default_graph()
-  sess_config <- list()
-  Sys.setenv(CUDA_VISIBLE_DEVICES = gpus)
-  sess_config$device_count <- list(GPU = 1L, CPU = 1L)
-  if (growth)
-    sess_config$gpu_options <- tf$GPUOptions(allow_growth = TRUE)
-  session_conf <- do.call(tf$ConfigProto, sess_config)
-  sess <- tf$Session(graph = tf$get_default_graph(), config = session_conf)
-  sess$run(tf$global_variables_initializer())
-  return(sess)
-}
+startGPUSession <- function(gpu.number, growth = T, env = F) {
+  # please note that CUDA_VISIBLE_DEVICES prevents processes from doing cudaMemcpy from/to devices not owned by the process. There's a significant performance degradation when NCCL is used with P2P communication disabled.
+  if (env)
+    Sys.setenv(CUDA_VISIBLE_DEVICES = gpu.number) 
 
-#' Ends the GPU sesstion
-#'
-#' @export
-end_gpu_session <- function(){
-  sess$close()
+  # get available GPUs on the machine as a list
+  available.gpus <- tensorflow::tf$config$experimental$get_visible_devices('GPU')
+  if (gpu.number >= length(available.gpus)) {
+    tensorflow::tf$config$experimental$set_visible_devices(available.gpus[[gpu.number]], 'GPU')
+  } else {
+    stop("gpu.number is out of bounds with GPUs on machine.")
+  }
+  if (growth)
+    tensorflow::tf$config$experimental$set_memory_growth(device = gpu, enable = TRUE)
 }
 
 list.local.devices <- function(){
-  message(tensorflow::tf$python$client$device_lib$list_local_devices())
+  message(tensorflow::tensorflow::tf$config$experimental$get_visible_devices('GPU'))
 }
 
 is.gpu.available <- function() {
